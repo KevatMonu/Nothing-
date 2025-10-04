7aca597d428771809c4830d15fcbd96a2464ca35
Monu Kenvat <monukevat3@gmail.com>
2025-10-04 23:01:51 +0530

    added animtionation and mores studd

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function lineanime() {
+  return gsap
+    .timeline({
+      defaults: {
+        ease: "power2.out",
+        duration: 0.5,
+        border: "1.5px solid #fff",
+      },
+    })
+    
+    .to(".hero-line1", { 
+        width: "9vw",
+         ease: "power3.out" 
+        })
+    .to(".hero-line2", { 
+        width: "15vw" 
+    }, "<0.25")
+    .to(".hero-line3", {
+         width: "8vw" }, "<0.25")
+    .to(".hero-line4", { width: "52vw" }, "<0.25")
+    .to(".hero-line5", { width: "15vw" }, "<0.25")
+    .to(".hero-line6", { width: "5vw" }, "<0.25")
+    .to(".hero-line7", { width: "5vw" }, "<0.25")
+    .to(".hero-line8", { width: "15vw" }, "<0.25")
+    .to(".hero-line9", { width: "5vw" }, "<0.25")
+    .to(".hero-line10", { width: "23vw" }, "<0.25")
+    .to(".hero-line11", { width: "50vw" }, "<0.25")
+    .to(".hero-line12", { width: "5vw" }, "<0.25")
+    .to(".hero-line13", { width: "46vw" }, "<0.25");
+}
+
+lineanime();
